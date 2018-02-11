afa7836c018ebe6d2d6880e5f6fa9d9c5b0e6351
mahicharla <35602067+mahicharla@users.noreply.github.com>
2018-02-11 10:27:20 +0000

    Add files via upload

---
+++ backend/test/CategoryDAOTest.java
@@ -0,0 +1,37 @@
+package com.niit.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.dao.CategoryDAO;
+import com.niit.model.Category;
+
+public class CategoryDAOTest {
+static CategoryDAO categoryDAO;
+	
+	@BeforeClass
+	public static void preExecution()
+	{
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
+		context.scan("com.niit");
+		context.refresh();
+		
+		categoryDAO=(CategoryDAO)context.getBean("categoryDAO");
+	}
+	
+	
+	@Test
+	public void addCategoryTest()
+	{
+		Category category=new Category();
+		category.setCategoryName("Samsung");
+		category.setCategoryDesc("Samsung Mobile");
+		
+		assertTrue("Problem in Adding Category",categoryDAO.addCategory(category));
+	}
+	
+
+}
